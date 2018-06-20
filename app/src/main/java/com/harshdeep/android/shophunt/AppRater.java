f5cc3cfb6c015b5abfb4d3195c029506b1b8f22e
Harshdeep Singh <hssahdev1999@gmail.com>
2018-06-20 21:43:28 +0530

    Adding app rater to display dialog box after 2 days

--- app/src/main/java/com/harshdeep/android/shophunt/AppRater.java
+++ app/src/main/java/com/harshdeep/android/shophunt/AppRater.java
@@ -1,4 +1,83 @@
 package com.harshdeep.android.shophunt;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+
 public class AppRater {
+
+    private final static String APP_TITLE = "ShopHunt";// App Name
+    private final static String APP_PNAME = "com.harshdeep.android.shophunt";// Package Name
+
+    private final static int DAYS_UNTIL_PROMPT = 2;//Min number of days
+    private final static int LAUNCHES_UNTIL_PROMPT = 3;//Min number of launches
+
+    public static void app_launched(Context mContext) {
+        SharedPreferences prefs = mContext.getSharedPreferences(mContext.getResources().getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+        if (prefs.getBoolean("dontshowagain", false)) {
+            return;
+        }
+
+        SharedPreferences.Editor editor = prefs.edit();
+
+        // Increment launch counter
+        long launch_count = prefs.getLong("launch_count", 0) + 1;
+        editor.putLong("launch_count", launch_count);
+
+        // Get date of first launch
+        Long date_firstLaunch = prefs.getLong("date_firstlaunch", 0);
+        if (date_firstLaunch == 0) {
+            date_firstLaunch = System.currentTimeMillis();
+            editor.putLong("date_firstlaunch", date_firstLaunch);
+        }
+
+        // Wait at least n days before opening
+        if (launch_count >= LAUNCHES_UNTIL_PROMPT) {
+            if (System.currentTimeMillis() >= date_firstLaunch +
+                    (DAYS_UNTIL_PROMPT * 24 * 60 * 60 * 1000)) {
+                showRateDialog(mContext, editor);
+            }
+        }
+
+        editor.apply();
+    }
+
+    public static void showRateDialog(final Context mContext, final SharedPreferences.Editor editor) {
+
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+
+        builder.setTitle("Spread the word?");
+        builder.setMessage("If you enjoy using " + APP_TITLE + ", please take a moment to rate it. Thanks for your support!");
+        builder.setPositiveButton("Rate", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                mContext.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + APP_PNAME)));
+                dialogInterface.dismiss();
+            }
+        });
+
+        builder.setNegativeButton("not now", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+
+        builder.setNeutralButton("no, thanks", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (editor != null) {
+                    editor.putBoolean("dontshowagain", true);
+                    editor.apply();
+                }
+                dialogInterface.dismiss();
+            }
+        });
+
+        builder.create().show();
+    }
 }
